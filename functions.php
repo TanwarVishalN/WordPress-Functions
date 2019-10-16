e78863cdf4b96fa6c5e8f594710aa269e17fc058
Vishal Tanwar <37098248+TanwarVishalN@users.noreply.github.com>
2019-10-16 15:02:06 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,17 +3,17 @@
   ----------Remove Type Attribute from wp_localized_script Start------------
   --------------------------------------------------------------------------*/
   
-add_action('wp_loaded', 'prefix_output_buffer_start');
-function prefix_output_buffer_start() { 
-    ob_start("prefix_output_callback"); 
+add_action('wp_loaded', 'myplugin_output_buffer_start');
+function myplugin_output_buffer_start() { 
+    ob_start("myplugin_output_callback"); 
 }
 
-add_action('shutdown', 'prefix_output_buffer_end');
-function prefix_output_buffer_end() { 
+add_action('shutdown', 'myplugin_output_buffer_end');
+function myplugin_output_buffer_end() { 
     ob_end_flush(); 
 }
 
-function prefix_output_callback($buffer) {
+function myplugin_output_callback($buffer) {
     return preg_replace( "%[ ]type=[\'\"]text\/(javascript|css)[\'\"]%", '', $buffer );
 }
 /*--------------------------------------------------------------------------
@@ -24,10 +24,10 @@ function prefix_output_callback($buffer) {
   -----------Remove Type Attribute from wp_enqueue_script Start-------------
   --------------------------------------------------------------------------*/
   
-add_filter('style_loader_tag', 'ckwebsiteDesign_remove_type_attr', 10, 2);
-add_filter('script_loader_tag', 'ckwebsiteDesign_remove_type_attr', 10, 2);
+add_filter('style_loader_tag', 'myplugin_remove_type_attr', 10, 2);
+add_filter('script_loader_tag', 'myplugin_remove_type_attr', 10, 2);
 
-function ckwebsiteDesign_remove_type_attr($tag, $handle) {
+function myplugin_remove_type_attr($tag, $handle) {
     return preg_replace( "/type=['\"]text\/(javascript|css)['\"]/", '', $tag );
 }
 
